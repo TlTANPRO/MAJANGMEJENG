d3d4ada617970d190954a004befd02686e2cf532
TlTANPRO <nickasad10007@gmail.com>
2026-08-15 17:14:17 +0700

    feat(v9): switch production entry to V9

--- artifacts/majang-mejeng/src/main.tsx
+++ artifacts/majang-mejeng/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './V8';
+import App from './V9';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
