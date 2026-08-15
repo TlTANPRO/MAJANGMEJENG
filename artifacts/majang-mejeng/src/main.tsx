908dadb454479a8e3e690cfdeae97a75966a4b73
TlTANPRO <nickasad10007@gmail.com>
2026-08-15 14:17:09 +0700

    Use V6 stylesheet as single source of truth

--- artifacts/majang-mejeng/src/main.tsx
+++ artifacts/majang-mejeng/src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import App from './V6';
 import './index.css';
-import './v6-motion.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
