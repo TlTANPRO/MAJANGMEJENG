4932f4fc36bd88251c415a66b5919e35c12c2fed
TlTANPRO <nickasad10007@gmail.com>
2026-08-15 17:01:03 +0700

    feat: switch production entrypoint to V8

--- artifacts/majang-mejeng/src/main.tsx
+++ artifacts/majang-mejeng/src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './V7';
+import App from './V8';
 import './index.css';
-import './v7-motion.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
