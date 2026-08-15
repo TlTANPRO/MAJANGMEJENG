73c0dd0600e56740a3e66999234834aedfeebe17
TlTANPRO <nickasad10007@gmail.com>
2026-08-15 14:28:25 +0700

    feat(v7): promote V7 as application entry and motion layer

--- artifacts/majang-mejeng/src/main.tsx
+++ artifacts/majang-mejeng/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import App from './V6';
+import App from './V7';
 import './index.css';
+import './v7-motion.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
