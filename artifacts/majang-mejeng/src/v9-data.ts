57c374af21445bb940695e086d8e1b458306358c
TlTANPRO <nickasad10007@gmail.com>
2026-08-15 17:15:02 +0700

    chore(v9): define production content and social contracts

---
+++ artifacts/majang-mejeng/src/v9-data.ts
@@ -0,0 +1,2 @@
+export const socialLinks={instagram:'https://www.instagram.com/majangmejeng_/',tiktok:'https://www.tiktok.com/@majangmejeng_?lang=id-ID'} as const;
+export const contentPolicy={placeholderImages:false,fabricatedSocialMetrics:false,brokenExternalMediaFallback:true,sourceRequired:true} as const;
