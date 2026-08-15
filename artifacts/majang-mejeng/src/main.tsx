b8ad28b4d9307282fbaf2e5dec7f2feb1f5a059a
TlTANPRO <nickasad10007@gmail.com>
2026-08-15 22:43:59 +0700

    feat(majang): load V13 motion system

--- artifacts/majang-mejeng/src/main.tsx
+++ artifacts/majang-mejeng/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client';
 import App from './V13';
 import './index.css';
 import './v12.css';
+import './v13.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
